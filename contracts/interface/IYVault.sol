dd8b5fc7e19fc270e3bb0634947bbc16ce7c1927
SuhailG <github@suhailg.me>
2022-02-27 06:34:59 -0700

    Update IYVault.sol

--- contracts/interface/IYVault.sol
+++ contracts/interface/IYVault.sol
@@ -3,16 +3,19 @@
 pragma solidity 0.8.11;
 
 interface IYVault {
-    function deposit(uint256) external;
+    function deposit(uint256) external returns (uint256);
 
-    function withdraw(uint256) external;
+    function withdraw(uint256) external returns (uint256);
 
-    function getPricePerFullShare() external view returns (uint256);
+    function withdraw(uint256, address) external returns (uint256);
 
     function token() external view returns (address);
 
-    // V2
     function pricePerShare() external view returns (uint256);
 
     function balanceOf(address owner) external view returns (uint256);
+
+    function totalAssets() external view returns (uint256);
+
+    function totalSupply() external view returns (uint256);
 }
