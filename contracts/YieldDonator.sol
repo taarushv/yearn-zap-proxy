b70a8daa5044b94a2254857ec00fad852bfa6b5f
SuhailG <github@suhailg.me>
2022-02-27 06:35:51 -0700

    Update YieldDonator
    - Add harvest and withdraw functions
    - TODO: comments
    - TODO: tests

--- contracts/YieldDonator.sol
+++ contracts/YieldDonator.sol
@@ -2,30 +2,49 @@
 
 pragma solidity 0.8.11;
 
-import "./solmate/SafeTransferLib.sol";
+import "./libraries/solmate/SafeTransferLib.sol";
+import "./libraries/prb-math/PRBMath.sol";
+import "./libraries/oz/Ownable.sol";
 import "./interface/IYVault.sol";
 import "./interface/IWETH.sol";
 
-contract YieldDonator {
+contract YieldDonator is Ownable {
     using SafeTransferLib for address;
     using SafeTransferLib for ERC20;
 
-    address public immutable donationAddress;
+    event Deposit(address indexed owner, uint256 amount);
+
+    event Harvest(address indexed harvester, uint256 amount);
+
+    event Withdraw(address indexed owner, uint256 amount);
 
     IYVault public immutable yearnVault;
 
     address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
+    address public immutable donationAddress;
+
+    mapping(address => bool) public acceptedDonationTokens;
+
+    mapping(address => bool) public harvesters;
+
     mapping(address => bool) public targets;
 
     mapping(address => uint256) public balanceOf;
 
-    bool public stopped;
+    uint256 public totalUnderlyingDeployed;
 
-    uint256 public totalDeployed;
+    bool public paused;
+
+    bool public openHarvest;
 
     modifier pausable() {
-        require(!stopped, "Paused");
+        require(!paused, "Paused");
+        _;
+    }
+
+    modifier onlyHarvesters() {
+        require(openHarvest || harvesters[msg.sender], "Invalid harvester");
         _;
     }
 
@@ -42,8 +61,9 @@ contract YieldDonator {
         address buyToken,
         address target,
         bytes calldata data,
-        uint256 minTokensRec
+        uint256 minYTokens
     ) external payable pausable {
+        require(buyToken == yearnVault.token(), "Invalid buyToken");
         sellAmount = _pull(sellToken, sellAmount);
 
         uint256 buyAmount = _execute(
@@ -55,17 +75,67 @@ contract YieldDonator {
         );
 
         balanceOf[msg.sender] += buyAmount;
+        totalUnderlyingDeployed += buyAmount;
 
         uint256 yTokensRec = _deposit(buyToken, buyAmount);
 
-        require(yTokensRec >= minTokensRec, "High slippage");
-
-        totalDeployed += yTokensRec;
+        require(yTokensRec >= minYTokens, "High slippage");
     }
 
-    function withdraw() external {}
+    function withdraw(uint256 amount) external {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        totalUnderlyingDeployed -= amount;
 
-    function harvest() external {}
+        uint256 vaultAssets = yearnVault.totalAssets();
+        uint256 totalSupply = yearnVault.totalSupply();
+
+        uint256 withdrawAmount = PRBMath.mulDiv(
+            amount,
+            totalSupply,
+            vaultAssets
+        );
+
+        yearnVault.withdraw(withdrawAmount, msg.sender);
+    }
+
+    function harvest(
+        address buyToken,
+        address target,
+        bytes calldata data,
+        uint256 minBuyAmount
+    ) external onlyHarvesters {
+        require(acceptedDonationTokens[buyToken], "Invalid buyToken");
+
+        uint256 vaultAssets = yearnVault.totalAssets();
+        uint256 totalSupply = yearnVault.totalSupply();
+
+        uint256 totalUnderlying = PRBMath.mulDiv(
+            yearnVault.balanceOf(address(this)),
+            vaultAssets,
+            totalSupply
+        );
+        uint256 yieldEarned = totalUnderlying - totalUnderlyingDeployed;
+        uint256 yieldToHarvest = PRBMath.mulDiv(
+            yieldEarned,
+            totalSupply,
+            vaultAssets
+        );
+
+        uint256 sellAmount = yearnVault.withdraw((yieldToHarvest));
+
+        uint256 buyAmount = _execute(
+            yearnVault.token(),
+            buyToken,
+            sellAmount,
+            target,
+            data
+        );
+
+        require(buyAmount > minBuyAmount, "High slippage");
+
+        ERC20(buyToken).safeTransfer(donationAddress, buyAmount);
+    }
 
     function _pull(address token, uint256 quantity) internal returns (uint256) {
         if (token == address(0)) {
@@ -123,14 +193,40 @@ contract YieldDonator {
         internal
         returns (uint256 yTokensRec)
     {
-        uint256 intialBalance = yearnVault.balanceOf(address(this));
-
         ERC20 _depositToken = ERC20(depositToken);
         _depositToken.approve(address(yearnVault), 0);
         _depositToken.approve(address(yearnVault), depositAmount);
 
-        yearnVault.deposit(depositAmount);
+        yTokensRec = yearnVault.deposit(depositAmount);
+    }
 
-        yTokensRec = yearnVault.balanceOf(address(this)) - intialBalance;
+    function updateTargets(address target, bool allowed) external onlyOwner {
+        targets[target] = allowed;
+    }
+
+    function updateAcceptedDonationTokens(address donationToken, bool allowed)
+        external
+        onlyOwner
+    {
+        acceptedDonationTokens[donationToken] = allowed;
+    }
+
+    function updateHarvesters(address harvester, bool allowed)
+        external
+        onlyOwner
+    {
+        harvesters[harvester] = allowed;
+    }
+
+    function togglePaused() external onlyOwner {
+        paused = !paused;
+    }
+
+    function toggleOpenHarvest() external onlyOwner {
+        openHarvest = !openHarvest;
+    }
+
+    receive() external payable {
+        require(msg.sender != tx.origin);
     }
 }
